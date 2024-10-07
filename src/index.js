ce62c862b5ba959d2b2a9d656224d7bdf06f2794
sherief-ibunsali <mdsheriefibunsali@gmail.com>
2024-10-07 16:04:05 +0530

    files added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import RouterReact from "./Nav/Route";
-import { Success } from "./response-results/success.js";
+import RouterReact from "./Nav/Route";
+// import { Success } from "./response-results/success.js";
 // import { Fail } from "./components/fail";
 // import Bill from "./components/bill";
 
 function RestaurantApp() {
-  // return <RouterReact />;
-  return <Success />;
+  return <RouterReact />;
+  // return <Success />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
